541e9bbe0389d8ab266714f442802ebfbb777488
Grégoire Body <bodygreg@yahoo.fr>
2026-07-16 18:01:47 +0200

    fix: Résolution password oublié 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,7 +26,13 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 // Middlewares
-app.use(cors())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://vite-gourmand-lemon.vercel.app'
+  ],
+  credentials: true
+}))
 app.use(express.json())
 
 // Connexion MongoDB
